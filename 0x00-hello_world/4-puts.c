3294be32c13662f5ed67ce2dc2f16bcad1529191
Leohnardoh <93118236+Leohnardoh@users.noreply.github.com>
2022-03-10 21:55:03 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,22 +3,10 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
- *  * main - prints the phrase "Programming is like building a multilingual puz
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
-
-=======
  * Return: Always 0 (Success)
  */
->>>>>>> 0eff7e23ac643cc14648e8dbfb05984a6df6db80
 int main(void)
 {
-<<<<<<< HEAD
 	puts("\"Programming is like building a multilingual puzzle");
-=======
- 	puts("\"Programming is like building a multilingual puzzle");
->>>>>>> 0eff7e23ac643cc14648e8dbfb05984a6df6db80
 	return (0);
 }
