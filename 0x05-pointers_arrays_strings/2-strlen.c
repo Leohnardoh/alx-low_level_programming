22f657786de3692d8f110248412def348c6d3153
Leohnardoh <93118236+Leohnardoh@users.noreply.github.com>
2022-03-21 15:06:41 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
-j gbiknghb
+#include "main.h"
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: length of the string passed to it
+*/
+
+
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
